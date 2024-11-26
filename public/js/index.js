e052f1608b7b0c38fb8a2ec97bee01bd96b8ee72
Corneliu Procopenco <procorneliu@gmail.com>
2024-11-26 19:35:37 +0200

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,7 @@ import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { displayMap } from './mapbox';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTES
 const mapBox = document.getElementById('map');
@@ -67,3 +68,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,7 +5,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(viewController.alerts);
+
 router.get('/', authController.isLoggedIn, viewController.getOverview);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
