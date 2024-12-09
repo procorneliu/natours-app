cd48b852b55c503e4d766375148c1fa5b99d72ff
Corneliu Procopenco <procorneliu@gmail.com>
2024-12-09 17:46:48 +0200

    Updating booking controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -92,14 +92,19 @@ const createBookingCheckout = async session => {
     await Booking.create({ tour, user, price });
 
     const customFieldValue = session.custom_fields[0].dropdown.value;
-    Tour.findOneAndUpdate(
+    const result = Tour.findOneAndUpdate(
       { _id: tour },
       {
         $set: {
           'startDates.1.participants': 2,
         },
       },
+      {
+        new: true,
+        runValidators: true,
+      },
     );
+    console.log(result);
   } catch (err) {
     console.log('Error creating booking:', err);
   }
